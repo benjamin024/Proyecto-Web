5096887d42bad5a06cde14461b4cc32fc291ebbe
benjamin024 <benjamin.dorantes@hotmail.com>
2016-11-10 16:50:05 -0600

    Login y registro de usuarios

---
+++ src/java/paquete/login.java
@@ -0,0 +1,78 @@
+package paquete;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import org.jdom.Document;
+import org.jdom.Element;
+import org.jdom.input.SAXBuilder;
+
+public class login extends HttpServlet {
+    
+    private int tipoUsuario = -1;
+    private int activo = -1;
+    private String rutaXML;
+    
+    private boolean usuarioEncontrado(String usr, String pass) throws Exception{
+        SAXBuilder builder = new SAXBuilder(); 
+        Document doc = builder.build(new FileInputStream(rutaXML + "/usuarios.xml"));
+                // Obtenemos la etiqueta raíz  
+        Element raiz = doc.getRootElement();  
+        // Recorremos los hijos de la etiqueta raíz  
+        List<Element> hijosRaiz = raiz.getChildren();  
+        for(Element hijo: hijosRaiz){  
+            String usuario = hijo.getAttributeValue("id");
+            String password = hijo.getChildText("password");
+            if(usuario.equals(usr) && password.equals(pass)){
+                tipoUsuario = Integer.parseInt(hijo.getAttributeValue("tipo"));
+                if(tipoUsuario == 3)
+                    activo  = Integer.parseInt(hijo.getAttributeValue("activo"));
+                return true;
+            }
+        } 
+        return false;
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+            String usuario = request.getParameter("user");
+            String pass = request.getParameter("pass");
+            response.setContentType("text/html;charset=UTF-8");
+            rutaXML = request.getRealPath("/") + "xml";
+            PrintWriter out=response.getWriter();
+            try {
+                boolean ok = usuarioEncontrado(usuario, pass);
+                if(ok){ //usuario y contraseña correctas
+                    switch(tipoUsuario){
+                        case 1:
+                            out.println("Bienvenido " + usuario);
+                            break;
+                        case 2:
+                            out.println("Bienvenido " + usuario);
+                            break;
+                        case 3:
+                            if(activo == 1){
+                                out.println("Bienvenido " + usuario);
+                            }else{
+                                out.println("Usuario Inactivo");
+                            }
+                    }
+                }else{
+                    out.println("Usuario o contraseña incorrectos");
+                }
+            } catch (Exception ex) {
+                Logger.getLogger(login.class.getName()).log(Level.SEVERE, null, ex);
+            }
+    }
+
+    
+
+}
---
+++ src/java/paquete/registraAlumno.java
@@ -0,0 +1,83 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package paquete;
+
+import java.io.FileInputStream;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import org.jdom.Document;
+import org.jdom.Element;
+import org.jdom.input.SAXBuilder;
+import org.jdom.output.Format;
+import org.jdom.output.XMLOutputter;
+
+
+public class registraAlumno extends HttpServlet {
+    
+    private String rutaXML;
+    
+    private boolean registroAlumno(String nom, String grup, String user, String email) throws Exception{
+        SAXBuilder builder = new SAXBuilder(); 
+        Document doc = builder.build(new FileInputStream(rutaXML + "/usuarios.xml"));
+        Element raiz = doc.getRootElement();
+        Element usuario = new Element("usuario");
+        Element nombre = new Element("nombre");
+        Element grupo = new Element ("grupo");
+        Element mail = new Element("mail");
+        Element pass = new Element("password");
+        nombre.setText(nom);
+        grupo.setText(grup);
+        mail.setText(email);
+        pass.setText(generaPassword(nom,grup));
+        usuario.setAttribute("id", user);
+        usuario.setAttribute("activo", "0");
+        usuario.setAttribute("tipo", "3");
+        usuario.addContent(nombre);
+        usuario.addContent(grupo);
+        usuario.addContent(mail);
+        usuario.addContent(pass);
+        raiz.addContent(usuario);
+        XMLOutputter xmlOutput = new XMLOutputter();
+        xmlOutput.setFormat(Format.getPrettyFormat());
+        xmlOutput.output(doc, new FileWriter(rutaXML + "/usuarios.xml"));
+        return true;
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+            response.setContentType("text/html;charset=UTF-8");
+            PrintWriter out = response.getWriter();
+            String nombre = request.getParameter("nombre");
+            String grupo = request.getParameter("grupo");
+            String user = request.getParameter("user");
+            String mail = request.getParameter("mail");
+            rutaXML = request.getRealPath("/") + "xml";
+            try {
+                boolean ok = registroAlumno(nombre, grupo, user, mail);
+                if(ok){
+                    out.println("Usuario registrado");
+                }else{
+                    out.println("ERROR");
+                }
+            } catch (Exception ex) {
+                Logger.getLogger(registraAlumno.class.getName()).log(Level.SEVERE, null, ex);
+            }
+            
+    }
+
+    private String generaPassword(String nom, String grup) {
+        String [] nombre = nom.split(" ");
+        return nombre[0].charAt(0) + nombre[1] + nombre[2].charAt(0) + grup.charAt(grup.length() - 2) + grup.charAt(grup.length() - 1);
+    }
+}
